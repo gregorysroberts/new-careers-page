74730c46a7aa0d3d94314d1ff6f54979c600bec3
Greg Roberts <gregorysroberts@gmail.com>
2014-01-21 11:42:55 -0700

    Adds function to display positions when a department is clicked, adds styling to displayed positions

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 $(function() {
 
 	$('.department').on('click', function (){
-		$('.department li').css('display', 'block');
+		$('.department li').css('display', 'none');
+		$(this).find('li').css('display', 'block');
 	});
 
 
