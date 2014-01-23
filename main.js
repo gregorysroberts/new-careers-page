974973fa4c73e910fabecb93ee5ae99401834d02
Greg Roberts <gregorysroberts@gmail.com>
2014-01-23 15:28:21 -0700

    Reverts to old styles from yesterday

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 $(function() {
 
-	$('.department').on('click', function (){
-		$('.department li').css('display', 'none');
-		$(this).find('li').css('display', 'block');
-	});
+        $('.department').on('click', function (){
+                $('.department li').css('display', 'none');
+                $(this).find('li').css('display', 'block');
+        });
 
-	$('#open-positions p').on('click', function (){
-		$('.department li').css('display', 'block');
-	});
+        $('#open-positions p').on('click', function (){
+                $('.department li').css('display', 'block');
+        });
 
 });
